da990c86ee5ae15966e521a3ee139674f5e02e26
hobsonsami42 <shobson@csumb.edu>
2025-07-24 09:56:41 -0700

    create LoginActivity UI

---
+++ app/src/main/java/com/example/cst_338_project_2_su_25/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.cst_338_project_2_su_25;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText usernameInput, passwordInput;
+    Button loginButton, signupButton;
+}
