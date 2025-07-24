d71bfede8bfee1d155834cb62e0874d4e344317f
hobsonsami42 <shobson@csumb.edu>
2025-07-24 09:43:10 -0700

    added JavaDoc comments to User entity fields

--- app/src/main/java/com/example/cst_338_project_2_su_25/entities/User.java
+++ app/src/main/java/com/example/cst_338_project_2_su_25/entities/User.java
@@ -7,7 +7,10 @@ public class User {
     @PrimaryKey(autoGenerate = true)
     public int userId;
 
+    /** The user's display name for login purposes. */
     public String username;
+    /**The user's password for login purposes. */
     public String password;
+    /** Flag for identifying admin users. */
     public boolean isAdmin;
 }
