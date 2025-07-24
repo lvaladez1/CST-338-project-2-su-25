f8cd64d6d1a080bd00c8a0f9aec84877833da10c
hobsonsami42 <shobson@csumb.edu>
2025-07-24 09:44:15 -0700

    added JavaDoc comments to DAO methods for user insert and login

--- app/src/main/java/com/example/cst_338_project_2_su_25/database/UserDao.java
+++ app/src/main/java/com/example/cst_338_project_2_su_25/database/UserDao.java
@@ -6,11 +6,27 @@ import androidx.room.Query;
 
 import com.example.cst_338_project_2_su_25.entities.User;
 
+/**
+ * Data Access Object (DAO) for User entity.
+ * Provides methods to interact with the User table in the database.
+ */
 @Dao
 public interface UserDao {
+    /**
+     * Inserts a new user into the User table.
+     *
+     * @param user The User object to be inserted.
+     */
     @Insert
     void insertUser(User user);
 
+    /**
+     * Retrieves a user from the User table based on username and password.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return The User object if found, null otherwise.
+     */
     @Query("SELECT * FROM User WHERE username = :username AND password = :password LIMIT 1")
     User userLogin(String username, String password);
 }
