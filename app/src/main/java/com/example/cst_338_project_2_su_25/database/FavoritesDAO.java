ee8a212f6d9c2f6810a2d9cf1bc844345280865b
faith-thomas <faiththomas654@gmail.com>
2025-07-24 17:40:28 -0700

    -Updated getFavoritesForUser query to use userId with LiveData return type -Added javadoc comments to FavoritesDAO -Updated favorites table to connect foreign key userId, if user gets deleted so does associated favorites

--- app/src/main/java/com/example/cst_338_project_2_su_25/database/FavoritesDAO.java
+++ app/src/main/java/com/example/cst_338_project_2_su_25/database/FavoritesDAO.java
@@ -1,5 +1,6 @@
 package com.example.cst_338_project_2_su_25.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -11,14 +12,30 @@ import java.util.List;
 
 @Dao
 public interface FavoritesDAO {
+
+    /**
+     * Inserts a new favorite into the FAVORITES_TABLE.
+     *
+     * @param favorite The Favorites object to be added.
+     */
     @Insert
     void addFavorite(Favorites favorite);
+
+    /**
+     * Removes a favorite entry from the FAVORITES_TABLE.
+     *
+     * @param favorite The Favorites object to be deleted.
+     */
     @Delete
     void removeFavorite(Favorites favorite);
 
-    //TODO: wait for sami to create user table so I can integrate it
-    //correct query below...update later to return live date
-    //@Query("SELECT * FROM FAVORITES_TABLE WHERE userId = :userId")
-    //List<Favorites> getFavoritesForUser(int userId);
+    /**
+     * Retrieves the list of favorite titles for a specific user as LiveData.
+     *
+     * @param userId The ID of the user whose favorites should be returned.
+     * @return LiveData list of the user's favorite titles.
+     */
+    @Query("SELECT * FROM FAVORITES_TABLE WHERE userId = :userId")
+    LiveData<List<Favorites>> getFavoritesForUser(int userId);
 
 }
