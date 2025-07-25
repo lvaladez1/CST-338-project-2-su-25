764cfc54dbf25ac5fd1968d81950391b53afadbd
Luis Valadez <valadez140@gmail.com>
2025-07-25 15:15:59 -0700

    Added comments.

--- app/src/main/java/com/example/cst_338_project_2_su_25/database/MediaTitleDAO.java
+++ app/src/main/java/com/example/cst_338_project_2_su_25/database/MediaTitleDAO.java
@@ -9,11 +9,37 @@ import com.example.cst_338_project_2_su_25.entities.MediaTitle;
 
 import java.util.List;
 
+/**
+ * MediaTitleDAO.java
+ *
+ * <p> Data Access Object (DAO) for performing database operations on the
+ * MediaTitle entity.
+ *
+ * <p> This interface defines methods for inserting and retrieving media titles
+ * from the Room database. Room will automatically generate the implementation
+ * for this interface at compile time.
+ *
+ * <p> Each method is annotated with Room-specific annotations that determine how
+ * SQL operations should be handled.
+ */
 @Dao
 public interface MediaTitleDAO {
+
+    /**
+     * Inserts a MediaTitle object into the MEDIA_TABLE.
+     *
+     * <p> If a conflict occurs the existing entry will be replaced with the new one.
+     *
+     * @param mediaTitle the MediaTitle object to insert or replace.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(MediaTitle mediaTitle);
 
+    /**
+     * Retrieves all media titles from the MEDIA_TABLE.
+     *
+     * @return a List containing all MediaTitle objects stored in the database.
+     */
     @Query("SELECT * FROM " + RevuDatabase.MEDIA_TABLE)
     List<MediaTitle> getAllMediaTitles();
 
