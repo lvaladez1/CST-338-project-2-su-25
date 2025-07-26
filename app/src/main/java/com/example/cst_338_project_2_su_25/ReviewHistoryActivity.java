5570571518cc2bb82a507d43eb5fcd54f32d8502
hobsonsami42 <shobson@csumb.edu>
2025-07-25 22:04:42 -0700

    created ReviewHistoryActivity to show reviews

---
+++ app/src/main/java/com/example/cst_338_project_2_su_25/ReviewHistoryActivity.java
@@ -0,0 +1,29 @@
+package com.example.cst_338_project_2_su_25;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ReviewHistoryActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ReviewDao reviewDao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_history);
+
+        recyclerView = findViewById(R.id.reviewRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        reviewDao = RevuDatabase.getDatabase(getApplicationContext()).reviewDao();
+        int userId = getSharedPreferences("appPrefs", MODE_PRIVATE)
+                .getInt("userId", -1);
+
+        List<Review> reviews = reviewDao.getReviewsForUser(userId);
+        recyclerView.setAdapter(new ReviewAdapter(reviews));
+    }
+}
