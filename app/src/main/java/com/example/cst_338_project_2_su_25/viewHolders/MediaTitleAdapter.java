e6917248e55393b07387e82f0c0ba8696207ea7e
faith-thomas <faiththomas654@gmail.com>
2025-07-28 23:58:15 -0700

    added logic to adapter to only show heart if mediatitle is favorite connected it all together in displaymediaactivity

--- app/src/main/java/com/example/cst_338_project_2_su_25/viewHolders/MediaTitleAdapter.java
+++ app/src/main/java/com/example/cst_338_project_2_su_25/viewHolders/MediaTitleAdapter.java
@@ -1,17 +1,31 @@
 package com.example.cst_338_project_2_su_25.viewHolders;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 
+import com.example.cst_338_project_2_su_25.R;
 import com.example.cst_338_project_2_su_25.entities.MediaTitle;
+
+import java.util.Collections;
+import java.util.List;
 
 public class MediaTitleAdapter extends ListAdapter<MediaTitle, MediaTitleViewHolder> {
 
+    //tracking fav id's
+    private List<Integer> favoriteIds = Collections.emptyList();
     public MediaTitleAdapter(@NonNull DiffUtil.ItemCallback<MediaTitle> diffCallBack) {
         super(diffCallBack);
+    }
+
+    //gets used when user's fav id changes
+    public void setFavoriteIds(List<Integer> ids) {
+        this.favoriteIds = ids != null ? ids : Collections.emptyList();
+        notifyDataSetChanged();
     }
 
     @NonNull
@@ -24,6 +38,15 @@ public class MediaTitleAdapter extends ListAdapter<MediaTitle, MediaTitleViewHol
     public void onBindViewHolder(@NonNull MediaTitleViewHolder holder, int position) {
         MediaTitle current = getItem(position);
         holder.bind(current.toString());
+
+        Button heart = holder.itemView.findViewById(R.id.favoriteButton);
+
+        // Shows the heart button only if this ID is in favoriteIds
+        if (favoriteIds.contains(current.getMediaTitleId())) {
+            heart.setVisibility(View.VISIBLE);
+        } else {
+            heart.setVisibility(View.GONE);
+        }
     }
 
     public static class MediaTitleDiff extends DiffUtil.ItemCallback<MediaTitle> {
